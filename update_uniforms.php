ded6d8043e888e85fe90181f63d0d306109329e3
rabufernandezucl-star <rabu.fernandez.ucl@phinmaed.com>
2026-02-27 00:46:03 +0800

    Update update_uniforms.php

--- update_uniforms.php
+++ update_uniforms.php
@@ -1,25 +1,17 @@
 <?php
 require_once 'shared/config/db.php';
 
-if(isset($_POST['key'], $_POST['stock'], $_POST['status'])){
+$key = $_POST['key'];
+$stock = $_POST['stock'];
+$status = $_POST['status'];
 
-    $key = $_POST['key'];
-    $stock = $_POST['stock'];
-    $status = $_POST['status'];
+$stmt = $pdo->prepare("
+    UPDATE uniforms
+    SET stock = ?, status = ?
+    WHERE uniform_key = ?
+");
 
-    $stmt = $pdo->prepare("
-        UPDATE uniforms
-        SET stock = ?, status = ?
-        WHERE uniform_key = ?
-    ");
+$stmt->execute([$stock, $status, $key]);
 
-    if($stmt->execute([$stock, $status, $key])){
-        echo "Update Successful";
-    }else{
-        echo "Update Failed";
-    }
-
-}else{
-    echo "Invalid Request";
-}
+echo "Rows affected: " . $stmt->rowCount();
 ?>
