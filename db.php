d0f798cd9f66f47abcd157a0218e624ed4a398da
rabufernandezucl-star <rabu.fernandez.ucl@phinmaed.com>
2026-02-26 23:43:34 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,18 +1,19 @@
 <?php
 $host = "localhost";
-$username = "knights2025_knights_admin";  // ← tamang user
-$password = "M84Q.z]M~FjVO((!";
+$username = "knights2025";
+$password = "dk_knights_2026";
 $dbname = "knights2025_uniforms_system";
 
 try {
+
     $pdo = new PDO(
         "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
         $username,
-        $password,
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        $password
     );
 
-    echo "Connected successfully!";
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
