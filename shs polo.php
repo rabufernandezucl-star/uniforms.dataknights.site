baceba32759666fa5e69a26d432b00d0be31ee0b
rabufernandezucl-star <rabu.fernandez.ucl@phinmaed.com>
2026-02-27 12:03:43 +0800

    Update shs polo.php

--- shs polo.php
+++ shs polo.php
@@ -48,7 +48,7 @@ $sizes = ['XS','S','M','L','XL','XXL'];
 
 foreach ($sizes as $size):
 
-$key = 'shs_polo' . strtolower($size);
+$key = 'shs_polo_' . strtolower($size);
 $stock = $uniformData[$key]['stock'];
 $status = $uniformData[$key]['status'];
 $statusClass = ($status == 'Available') ? 'available' : 'not-available';
@@ -84,3 +84,4 @@ $statusClass = ($status == 'Available') ? 'available' : 'not-available';
 
 </table>
 </div>
+
