46131f12be6f8ef796a893f51f1527da395e5490
rabufernandezucl-star <rabu.fernandez.ucl@phinmaed.com>
2026-03-15 20:46:51 +0800

    Update delete_sale.php

--- delete_sale.php
+++ delete_sale.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 require 'db.php';
-if(!isset($_SESSION['username'])){ header('Location: login.php'); exit; }
+if(!isset($_SESSION['username'])){ header('Location: record_sale.php'); exit; }
 
 $id = $_GET['id'];
 $stmt = $pdo->prepare("DELETE FROM sales WHERE id=?");
 $stmt->execute([$id]);
 
-header('Location: dashboard.php'); exit;
+header('Location: record_sale.php'); exit;
