71e3824240080940531d8354e3c00ca24a8ab57d
rabufernandezucl-star <rabu.fernandez.ucl@phinmaed.com>
2026-03-21 16:53:04 +0800

    Update login.php

--- login.php
+++ login.php
@@ -32,7 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $_SESSION['username'] = $user['username'];
         $_SESSION['is_admin'] = $user['is_admin'];
 
-        header("Location: index.php");
+        header("Location: index");
         exit;
 
     } else {
